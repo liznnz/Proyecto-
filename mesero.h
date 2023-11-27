5c080999ddeba15672e49cae61d45a5930c5dd94
liznnz <149414880+liznnz@users.noreply.github.com>
2023-11-27 12:39:35 -0600

    Update mesero.h

--- mesero.h
+++ mesero.h
@@ -25,89 +25,96 @@
 class Mesero {
 
     public:
-    //Procedimiento de menu para tomar orden 
-    void tomarOrden(Tacos& tacos, Enchiladas& enchiladas, Pollo& pollo) {
-        int opcion;
-        do {
-            //Imprime las opciones que va a tener el sistema
-            std::cout << "\nMenu:\n" << std::endl;
-            std::cout << "1. Agrega Tacos" << std::endl;
-            std::cout << "2. Agrega Enchiladas" << std::endl;
-            std::cout << "3. Agrega Pollo" << std::endl;
-            std::cout << "4. Total" << std::endl;
-            std::cout << "\nSeleccione una opcion: ";
-            std::cin >> opcion;  //Leer indice que selecciona la opcion del menu
+        //Procedimiento de menu para tomar orden 
+        void tomarOrden(Tacos& tacos, Enchiladas& enchiladas, Pollo& pollo) {
+            int opcion;
+            do {
+                //Imprime las opciones que va a tener el sistema
+                std::cout << "\nMenu:\n" << std::endl;
+                std::cout << "1. Agrega Tacos" << std::endl;
+                std::cout << "2. Agrega Enchiladas" << std::endl;
+                std::cout << "3. Agrega Pollo" << std::endl;
+                std::cout << "4. Total" << std::endl;
+                std::cout << "\nSeleccione una opcion: ";
+                std::cin >> opcion;  //Leer indice que selecciona la opcion del menu
 
-            //Switch donde dependiendo la eleccion efectua un diferente procedimiento
-            switch (opcion) {
+                //Switch donde dependiendo la eleccion efectua un diferente procedimiento
+                switch (opcion) {
 
-                //Caso 1 que imprime las cantidades a agregar de tacos
-                case 1:
-                    agregarTacos(tacos);
-                    break;
-                //Caso 2 que imprime las cantidades a agregar de enchiladas
-                case 2:
-                    agregarEnchiladas(enchiladas);
-                    break;
-                //Caso 3 que imprime las cantidades a agregar de pollo
-                case 3:
-                    agregarPollo(pollo);
-                    break;
-                //Caso 4 que imprime el total de la orden
-                case 4:
-                    std::cout << "Orden tomada. ÁBuen provecho!\n";
-                    break;
-                //Instruccion para no aceptar numeros que no hay en el menu 
-                default:
-                    std::cout << "Opcion invalida. Intente de nuevo.\n";
-            }
-        //Ciclo para que el sistema siga corriendo
-        } while (opcion != 4);
+                    //Caso 1 que imprime las cantidades a agregar de tacos
+                    case 1:
+                        agregarTacos(tacos);
+                        break;
+                    //Caso 2 que imprime las cantidades a agregar de enchiladas
+                    case 2:
+                        agregarEnchiladas(enchiladas);
+                        break;
+                    //Caso 3 que imprime las cantidades a agregar de pollo
+                    case 3:
+                        agregarPollo(pollo);
+                        break;
+                    //Caso 4 que imprime el total de la orden y salir del ciclo
+                    case 4:
+                        std::cout << "Orden tomada. ÁBuen provecho!\n";
+                        break;
+                    //Mensaje para opciones no validas
+                    default:
+                        std::cout << "Opcion invalida. Intente de nuevo.\n";
+                }
+            //Ciclo para que el sistema siga corriendo
+            } while (opcion != 4);
     }
 
     private:
-    void agregarTacos(Tacos& tacos) {
-        int cantidadBistec, cantidadChorizo, cantidadTripita;
-        std::cout << "Ingrese la cantidad de Tacos de Bistec: ";
-        std::cin >> cantidadBistec;
-        tacos.setCantidadBistec(tacos.getCantidadBistec() + cantidadBistec);
 
-        std::cout << "Ingrese la cantidad de Tacos de Chorizo: ";
-        std::cin >> cantidadChorizo;
-        tacos.setCantidadChorizo(tacos.getCantidadChorizo() + cantidadChorizo);
+        //Procedimiento para agregar tacos a la orden
+        void agregarTacos(Tacos& tacos) {
+            int cantidadBistec, cantidadChorizo, cantidadTripita;
+            std::cout << "Ingrese la cantidad de Tacos de Bistec: ";
+            std::cin >> cantidadBistec;
+            tacos.setCantidadBistec(tacos.getCantidadBistec() + cantidadBistec);
 
-        std::cout << "Ingrese la cantidad de Tacos de Tripita: ";
-        std::cin >> cantidadTripita;
-        tacos.setCantidadTripita(tacos.getCantidadTripita() + cantidadTripita);
-    }
+            std::cout << "Ingrese la cantidad de Tacos de Chorizo: ";
+            std::cin >> cantidadChorizo;
+            tacos.setCantidadChorizo(tacos.getCantidadChorizo() + cantidadChorizo);
 
-    void agregarEnchiladas(Enchiladas& enchiladas) {
-        int cantidadEnchiladas;
-        std::cout << "Ingrese la cantidad de Enchiladas: ";
-        std::cin >> cantidadEnchiladas;
-        enchiladas.setCantidad(enchiladas.getCantidad() + cantidadEnchiladas);
-    }
+            std::cout << "Ingrese la cantidad de Tacos de Tripita: ";
+            std::cin >> cantidadTripita;
+            tacos.setCantidadTripita(tacos.getCantidadTripita() + cantidadTripita);
+        }
 
-    void agregarPollo(Pollo& pollo) {
-        int cantidadMedioPollo, cantidadPolloEntero, cantidadDosPiezas;
-        std::cout << "Ingrese la cantidad de Medio Pollo: ";
-        std::cin >> cantidadMedioPollo;
-        pollo.setCantidadMedioPollo(pollo.getCantidadMedioPollo() + cantidadMedioPollo);
 
-        std::cout << "Ingrese la cantidad de Pollo Entero: ";
-        std::cin >> cantidadPolloEntero;
-        pollo.setCantidadPolloEntero(pollo.getCantidadPolloEntero() + cantidadPolloEntero);
+        //Procedimiento para agregar enchiladas a la orden
+        void agregarEnchiladas(Enchiladas& enchiladas) {
+            int cantidadEnchiladas;
+            std::cout << "Ingrese la cantidad de Enchiladas: ";
+            std::cin >> cantidadEnchiladas;
+            enchiladas.setCantidad(enchiladas.getCantidad() + cantidadEnchiladas);
+        }
 
-        std::cout << "Ingrese la cantidad de Dos Piezas de Pollo: ";
-        std::cin >> cantidadDosPiezas;
-        pollo.setCantidadDosPiezas(pollo.getCantidadDosPiezas() + cantidadDosPiezas);
-    }
-};
+        //Procedimiento para agregar pollo a la orden
+        void agregarPollo(Pollo& pollo) {
+            int cantidadMedioPollo, cantidadPolloEntero, cantidadDosPiezas;
+            std::cout << "Ingrese la cantidad de Medio Pollo: ";
+            std::cin >> cantidadMedioPollo;
+            pollo.setCantidadMedioPollo(pollo.getCantidadMedioPollo() + cantidadMedioPollo);
+
+            std::cout << "Ingrese la cantidad de Pollo Entero: ";
+            std::cin >> cantidadPolloEntero;
+            pollo.setCantidadPolloEntero(pollo.getCantidadPolloEntero() + cantidadPolloEntero);
+
+            std::cout << "Ingrese la cantidad de Dos Piezas de Pollo: ";
+            std::cin >> cantidadDosPiezas;
+            pollo.setCantidadDosPiezas(pollo.getCantidadDosPiezas() + cantidadDosPiezas);
+        }
+}
 
 //Declaro objeto Contador que hereda de Mesero
 class Contador : public Mesero {
     public:
+    //Funcion para calcular el total de la orden 
     double calcularTotal(const Tacos& tacos, const Enchiladas& enchiladas, const Pollo& pollo) const {
+        //Calcula el total sumando los costos de cada tipo de comida multiplicado por la cantidad pedida
         return tacos.getCantidadBistec() * 8 + tacos.getCantidadChorizo() * 8 + tacos.getCantidadTripita() * 9 +
                enchiladas.getCantidad() * 35 + pollo.getCantidadMedioPollo() * 80 +
                pollo.getCantidadPolloEntero() * 160 + pollo.getCantidadDosPiezas() * 35;
