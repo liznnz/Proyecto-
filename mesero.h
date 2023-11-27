6dd286e32621edde6001e8bee0ab375db076b4ba
liznnz <149414880+liznnz@users.noreply.github.com>
2023-11-27 12:20:34 -0600

    Update mesero.h

--- mesero.h
+++ mesero.h
@@ -1,5 +1,5 @@
 /*
- * Clase Mesero que es para a–adir el pedido
+ * Clase Mesero que es para agregar el pedido
  * y tiene 1 clase hija para dar el total:
  * Contador
  *
@@ -13,45 +13,52 @@
 #include <iomanip>
 #include "comida.h"
 
+
+//Declaracion de clase empleado que es abstracta
 class Mesero {
-public:
+
+    public:
+    //Procedimiento de menu para tomar orden 
     void tomarOrden(Tacos& tacos, Enchiladas& enchiladas, Pollo& pollo) {
         int opcion;
         do {
-            // Menu
+            //Imprime las opciones que va a tener el sistema
             std::cout << "\nMenu:\n" << std::endl;
             std::cout << "1. Agrega Tacos" << std::endl;
             std::cout << "2. Agrega Enchiladas" << std::endl;
             std::cout << "3. Agrega Pollo" << std::endl;
             std::cout << "4. Total" << std::endl;
             std::cout << "\nSeleccione una opcion: ";
-            std::cin >> opcion;
+            std::cin >> opcion;  //Leer indice que selecciona la opcion del menu
 
+            //Switch donde dependiendo la eleccion efectua un diferente procedimiento
             switch (opcion) {
+
+                //Caso 1 que imprime las cantidades a agregar de tacos
                 case 1:
                     agregarTacos(tacos);
                     break;
-
+                //Caso 2 que imprime las cantidades a agregar de enchiladas
                 case 2:
                     agregarEnchiladas(enchiladas);
                     break;
-
+                //Caso 3 que imprime las cantidades a agregar de pollo
                 case 3:
                     agregarPollo(pollo);
                     break;
-
+                //Caso 4 que imprime el total de la orden
                 case 4:
                     std::cout << "Orden tomada. ÁBuen provecho!\n";
                     break;
-
+                //Instruccion para no aceptar numeros que no hay en el menu 
                 default:
                     std::cout << "Opcion invalida. Intente de nuevo.\n";
             }
-
+        //Ciclo para que el sistema siga corriendo
         } while (opcion != 4);
     }
 
-private:
+    private:
     void agregarTacos(Tacos& tacos) {
         int cantidadBistec, cantidadChorizo, cantidadTripita;
         std::cout << "Ingrese la cantidad de Tacos de Bistec: ";
