966f809609bdb92222325569149625a58611ac99
liznnz <149414880+liznnz@users.noreply.github.com>
2023-11-27 11:51:32 -0600

    Update mesero.h

--- mesero.h
+++ mesero.h
@@ -23,7 +23,7 @@ public:
             std::cout << "1. Agrega Tacos" << std::endl;
             std::cout << "2. Agrega Enchiladas" << std::endl;
             std::cout << "3. Agrega Pollo" << std::endl;
-            std::cout << "4. Salir" << std::endl;
+            std::cout << "4. Total" << std::endl;
             std::cout << "\nSeleccione una opcion: ";
             std::cin >> opcion;
 
